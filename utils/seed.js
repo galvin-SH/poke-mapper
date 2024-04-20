a987c4f4a68b0862a36ad20905db49dcc704751a
galvin-SH <mgalvin1993@gmail.com>
2024-04-20 13:10:42 -0700

    refactored queries to create evolution relationships between pokemon species

--- utils/seed.js
+++ utils/seed.js
@@ -2,6 +2,7 @@ const apiURL = 'https://pokeapi.co/api/v2/pokemon/';
 const axios = require('axios');
 const neo4j = require('../config/connection');
 
+// Seed the database with Pokemon data
 async function seedDatabase() {
     try {
         // Clear database
@@ -10,33 +11,61 @@ async function seedDatabase() {
         // Seed database
         const pokemonList = await getPokemonList();
         for (const pokemon of pokemonList) {
-            await seedPokemon(pokemon);
+            await seedEvolutions(pokemon);
         }
         console.log('Database seeded');
     } catch (error) {
         console.error('Error seeding database: ', error);
     }
 }
-
+// Get list of Pokemon from the PokeAPI
 async function getPokemonList() {
     const response = await axios.get(apiURL);
     return response.data.results;
 }
-
-async function seedPokemon(pokemon) {
+// Seed the database with Pokemon evolution data
+async function seedEvolutions(pokemon) {
     const response = await axios.get(pokemon.url);
     const pokemonData = response.data;
+    const speciesResponse = await axios.get(pokemonData.species.url);
+    const speciesData = speciesResponse.data;
+    const evolutionChainResponse = await axios.get(
+        speciesData.evolution_chain.url
+    );
+    const evolutionChainData = evolutionChainResponse.data.chain;
+    await seedEvolutionChain(evolutionChainData);
+}
+// Seed the database with Pokemon evolution chain data
+async function seedEvolutionChain(chain) {
     const query = `
-        MERGE (p:Pokemon {id: $id})
-        ON CREATE SET p.name = $name, p.type1 = $type1, p.type2 = $type2, p.height = $height, p.weight = $weight 
+        MERGE (e:Evolution {id: $id})
+        ON CREATE SET e.speciesURL = $speciesURL
     `;
     const params = {
-        id: pokemonData.id,
-        name: pokemonData.name,
-        type1: pokemonData.types[0].type.name,
-        type2: pokemonData.types[1] ? pokemonData.types[1].type.name : null,
-        height: pokemonData.height,
-        weight: pokemonData.weight,
+        id: chain.species.name,
+        speciesURL: chain.species.url,
+    };
+    await neo4j.executeQuery(query, params);
+    if (chain.evolves_to.length > 0) {
+        for (const evolution of chain.evolves_to) {
+            await seedEvolutionChain(evolution);
+            await seedEvolutionRelationship(
+                chain.species.name,
+                evolution.species.name
+            );
+        }
+    }
+}
+// Seed the database with Pokemon evolution relationship data
+async function seedEvolutionRelationship(species1, species2) {
+    const query = `
+        MATCH (e1:Evolution {id: $species1})
+        MATCH (e2:Evolution {id: $species2})
+        MERGE (e1)-[:EVOLVES_TO]->(e2)
+    `;
+    const params = {
+        species1,
+        species2,
     };
     await neo4j.executeQuery(query, params);
 }
